0187fb928bd6191923f33d23e26cfd85bb5eecfb
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-11-27 16:55:10 +0100

    created a database folder and db.sql file with some TODOs

---
+++ src/database/db.sql
@@ -0,0 +1,6 @@
+/*
+TODO db létrehozása
+TODO workers tábla létrehozása (adószám*, taj, név, szülidő, telszám, email, pass, belépés, munkavézési hely(FK), beosztás(FK), óraszám)
+TODO munkavégzési hely tábla (id*, név, irsz, város, cím, tel, email)
+TODO beosztás tábla (id*, megnevezés, jogosultsági szint)
+*/
