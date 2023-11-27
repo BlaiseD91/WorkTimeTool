6f61c6e9ce61870166733209bd7e043c55308972
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-11-27 20:28:13 +0100

    class updated and got a constructor

--- src/app/workplace.ts
+++ src/app/workplace.ts
@@ -3,6 +3,17 @@ export class WorkPlace {
     name?:string;
     zip?:number;
     city?:string;
+    address?:string;
     phone?:string;
     email?:string;
+
+    constructor(key:number, name:string, zip:number, city:string, address:string, phone:string, email:string) {
+        this.key = key;
+        this.name = name;
+        this.zip = zip;
+        this.city = city;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
 }
