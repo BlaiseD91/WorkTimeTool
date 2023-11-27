dc900f7662d35490b1b65c8573de002e717f8f17
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-11-27 20:05:36 +0100

    todo update

--- src/database/db.sql
+++ src/database/db.sql
@@ -1,6 +1,6 @@
 /*
 TODO db létrehozása
-TODO workers tábla létrehozása (adószám*, taj, név, szülidő, telszám, email, pass, belépés, munkavézési hely(FK), beosztás(FK), óraszám)
+TODO workers tábla létrehozása (adószám*, taj, név, szülidő, telszám, email, pass, belépés, munkavézési hely(FK), beosztás(FK), óraszám, aktív)
 TODO munkavégzési hely tábla (id*, név, irsz, város, cím, tel, email)
 TODO beosztás tábla (id*, megnevezés, jogosultsági szint)
 */
