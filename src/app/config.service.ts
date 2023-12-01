f86b06bd5e12233ad0d9e8a39d386de01f55d379
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-12-02 00:01:51 +0100

    setLanguage function add to set a language

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
-  constructor() { }
+  private configSub = new BehaviorSubject({});
+
+  constructor(private http:HttpClient) { 
+    this.setLanguage('hu')
+  }
+
+  setLanguage(lang:string) {
+    this.http.get('../assets/lang_' + lang + '.json').subscribe(
+      (response) => this.configSub.next(response)
+    )
+  }
+
+
+
 }
